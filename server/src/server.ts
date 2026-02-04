b8e7ef470f4f42690128df97ffcc7f36c0fc3377
Sudhanshu <sudhanshumani94@gmail.com>
2026-02-04 12:11:21 +0530

    Add production scripts

--- server/src/server.ts
+++ server/src/server.ts
@@ -14,7 +14,7 @@ const app:Application= express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
