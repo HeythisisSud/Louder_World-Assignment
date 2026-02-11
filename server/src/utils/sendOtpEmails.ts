09256c78b2f6793b1cfa7b656eff4e3be6f0e989
Sudhanshu <sudhanshumani94@gmail.com>
2026-02-11 18:48:27 +0530

    add otp auth with resend

--- server/src/utils/sendOtpEmails.ts
+++ server/src/utils/sendOtpEmails.ts
@@ -1,23 +1,12 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
+
 
 export async function sendOtpEmail(email: string, otp: string) {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-
-  await transporter.sendMail({
-    from: `"Sydney Events" <${process.env.EMAIL_USER}>`,
-    to: email,
-    subject: "Your OTP Code",
-    html: `
-      <h2>Your OTP Code</h2>
-      <p>Your verification code is:</p>
-      <h1>${otp}</h1>
-      <p>This code expires in 5 minutes.</p>
-    `,
-  });
+  const resend = new Resend(process.env.API_RESEND);
+  await resend.emails.send({
+  from: "onboarding@resend.dev",
+  to: email,
+  subject: "Your OTP",
+  text: `Your OTP is ${otp}`,
+});
 }
